d0d5a539228052d2c1a1ce894b7f358af44d07d0
mmaletskov <maletskovitis@mail.ru>
2023-03-18 10:51:02 +0100

    init

---
+++ src/test/java/ru/maletskov/calendar/CalendarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maletskov.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
