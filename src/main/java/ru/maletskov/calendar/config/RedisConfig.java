3381106d8672a7857a331e2123317392ca300aec
Hukutach <nikitachum99@gmail.com>
2023-04-04 21:12:56 +0300

    CA-10:Setup Redis Database

--- src/main/java/ru/maletskov/calendar/config/RedisConfig.java
+++ src/main/java/ru/maletskov/calendar/config/RedisConfig.java
@@ -1,36 +1,34 @@
 package ru.maletskov.calendar.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        RedisProperties properties = redisProperties();
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(properties.getHost());
-        redisStandaloneConfiguration.setPort(properties.getPort());
-
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
+        jedisConFactory.setHostName(redisHostName);
+        jedisConFactory.setPort(redisPort);
+        return jedisConFactory;
     }
 
     @Bean
-    RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-        return redisTemplate;
-    }
-
-    @Autowired
-    @Primary
-    RedisProperties redisProperties() {
-        return new RedisProperties();
-    }
+    public RedisTemplate<String, Object> redisTemplate() {
+            RedisTemplate<String, Object> template = new RedisTemplate<>();
+            template.setConnectionFactory(jedisConnectionFactory());
+            return template;
+            }
 }
